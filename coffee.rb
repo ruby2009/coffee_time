3d8336949a20c9a1d0483493e97775630c66046e
Ben Call <bgcall@indiana.edu>
2017-02-06 15:10:41 -0500

    First hw upload

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,19 @@
 class Coffee
-  
+
+attr_accessor :name, :full
+
+  def initialize(name, num=3)
+    @name = name
+    @full = num
+  end
+
+  def full?
+    full == 3
+  end
+
+  def empty?
+    full == 0
+  end
+
+
 end
--- human.rb
+++ human.rb
@@ -1,11 +1,32 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :item
 
   def initialize(name, alertness=0)
     self.name = name
     self.alertness = alertness
   end
 
+  def has_coffee?
+    item
+  end
+
+  def needs_coffee?
+    if item
+      then false
+    else
+      true
+    end
+  end
+
+  def buy(coffee)
+    self.item = coffee
+  end
+
+  def drink!
+    item.full -= 1
+    self.alertness += 0.32
+  end
 
 end
